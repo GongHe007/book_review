9e69bf893f9aa7d931bdae12f75d65a825fc06fa
gonghe <gonghe@creatingev.com>
2017-05-01 13:22:03 +0800

    add ui

--- app/controllers/api/books_controller.rb
+++ app/controllers/api/books_controller.rb
@@ -1,6 +1,11 @@
 class Api::BooksController < ApiController
   def index
-    @books = Book.order("updated_at desc")
+    if params[:content].present?
+      content = params[:content].gsub(" ", "")
+      @search_books = Book.where("name like ?", "%#{content}%")
+    end
+    @time_books = Book.order("updated_at desc").limit(12)
+    @review_books = Book.order("book.book_reviews desc")
   end
 
   def show
