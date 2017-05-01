d3dfeb6bdd93968746e404b37d103bf2b29fed63
gonghe <gonghe@creatingev.com>
2017-05-01 21:41:52 +0800

    add type

--- app/controllers/api/books_controller.rb
+++ app/controllers/api/books_controller.rb
@@ -1,12 +1,16 @@
 class Api::BooksController < ApiController
   def index
-    if params[:content].present?
-      content = params[:content].gsub(" ", "")
-      @search_books = Book.where("name like ?", "%#{content}%")
+    if params[:book_type]
+      @books = Book.where(book_type: params[:book_type])
+    else
+      if params[:content].present?
+        content = params[:content].gsub(" ", "")
+        @search_books = Book.where("name like ?", "%#{content}%")
+      end
+      @time_books = Book.order("created_at desc").limit(12)
+      @review_books = Book.order("book_reviews_count desc, created_at desc").limit(12)
+      @book_reviews = BookReview.where("title is not null").order("replies_count desc").limit(3)
     end
-    @time_books = Book.order("created_at desc").limit(12)
-    @review_books = Book.order("book_reviews_count desc, created_at desc").limit(12)
-    @book_reviews = BookReview.where("title is not null").order("replies_count desc").limit(3)
   end
 
   def show
