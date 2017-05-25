17a196730dbbffcacf367f4116f3ae30661e63eb
gonghe <gonghe@creatingev.com>
2017-05-25 17:35:34 +0800

    add new demands

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,3 +1,8 @@
 class Book < ActiveRecord::Base
   has_many :book_reviews
+  validates :name, presence: true, allow_blank: false
+  validates :author, presence: true, allow_blank: false
+  validates :description, presence: true, allow_blank: false
+  validates :img, presence: true, allow_blank: false
+  validates :book_type, presence: true, allow_blank: false
 end
